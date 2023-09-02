906402ed6ee4a259dff22fed1c24fae0d3946f33
Marcos Sousa <marcosp.sousa@gmail.com>
2023-09-02 19:52:59 -0300

    refactoring and defined category constructor

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,10 +1,7 @@
-import {omit} from 'lodash';
-import UniqueEntityId from "../../../@shared/domain/value-objects/unique-entity-id";
 import Category from "./category";
+import {omit} from "lodash";
 
-
-
-describe('Category Tests', () => {
+describe('Category Unit Test', () => {
 
     beforeEach(() => {
         Category.validate = jest.fn();
@@ -15,139 +12,53 @@ describe('Category Tests', () => {
         let props = omit(category.props, 'createdAt');
 
         expect(Category.validate).toHaveBeenCalled();
-
         expect(props).toStrictEqual({
             name: 'Movie',
-            isActive: true,
             description: null,
+            isActive: true,
         });
-        expect(category.createdAt).toBeInstanceOf(Date);
+        expect(category.props.createdAt).toBeInstanceOf(Date);
 
         let createdAt = new Date();
-
-        category = new Category({name: 'Movie', isActive: false, description: 'Movie category', createdAt});
+        category = new Category({
+            name: 'Movie',
+            description: 'Movie description',
+            isActive: false,
+            createdAt,
+        });
         expect(category.props).toStrictEqual({
             name: 'Movie',
+            description: 'Movie description',
             isActive: false,
-            description: 'Movie category',
             createdAt,
         });
 
-        category = new Category({name: 'Movie', description: 'Other movie category'});
+        category = new Category({
+            name: 'Movie',
+            description: 'other description',
+        });
         expect(category.props).toMatchObject({
             name: 'Movie',
-            description: 'Other movie category',
+            description: 'other description',
         });
 
-        category = new Category({name: 'Movie', isActive: true});
+        category = new Category({
+            name: 'Movie',
+            isActive: true,
+        });
         expect(category.props).toMatchObject({
             name: 'Movie',
             isActive: true,
         });
 
-        category = new Category({name: 'Movie', createdAt: createdAt});
+        createdAt = new Date();
+        category = new Category({
+            name: 'Movie',
+            createdAt,
+        });
         expect(category.props).toMatchObject({
             name: 'Movie',
-            createdAt
+            createdAt,
         });
     });
-
-    test('id field', () => {
-        let category = new Category({name: 'Movie'});
-        expect(category.id).not.toBeNull();
-        expect(category.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
-
-        category = new Category({name: 'Movie'}, null);
-        expect(category.id).not.toBeNull();
-        expect(category.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
-
-        category = new Category({name: 'Movie'}, undefined);
-        expect(category.id).not.toBeNull();
-        expect(category.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
-
-        category = new Category({name: 'Movie'}, new UniqueEntityId('123e4567-e89b-12d3-a456-426614174000'));
-        expect(category.id).not.toBeNull();
-        expect(category.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
-    });
-
-    test('name getter and setter', () => {
-        const category = new Category({name: 'Movie'});
-        expect(category.name).toBe('Movie');
-
-        category['name'] = 'Other movie';
-        expect(category.name).toBe('Other movie');
-    });
-
-    test('description getter and setter', () => {
-        let category = new Category({name: 'Movie'});
-        expect(category.description).toBeNull();
-
-        category = new Category({name: 'Movie', description: 'Movie category'});
-        expect(category.description).toBe('Movie category');
-
-        category['description'] = 'Other movie category';
-        expect(category.description).toBe('Other movie category');
-
-        category['description'] = undefined;
-        expect(category.description).toBeNull();
-
-        category['description'] = null;
-        expect(category.description).toBeNull();
-    });
-
-    test('isActive getter', () => {
-        let category = new Category({name: 'Movie'});
-        expect(category.isActive).toBeTruthy();
-
-        category = new Category({name: 'Movie', isActive: true});
-        expect(category.isActive).toBeTruthy();
-
-        category = new Category({name: 'Movie', isActive: false});
-        expect(category.isActive).toBeFalsy();
-
-        category = new Category({name: 'Movie', isActive: undefined});
-        expect(category.isActive).toBeTruthy();
-
-        category = new Category({name: 'Movie', isActive: null});
-        expect(category.isActive).toBeTruthy();
-    });
-
-    test('createdAt getter', () => {
-        let category = new Category({name: 'Movie'});
-        expect(category.createdAt).toBeInstanceOf(Date);
-
-        const createdAt = new Date();
-        category = new Category({name: 'Movie', createdAt});
-        expect(category.createdAt).toBe(createdAt);
-    });
-
-    test('update method', () => {
-        const category = new Category({name: 'Movie'});
-        category.update('Other movie', 'Other movie category');
-
-        expect(Category.validate).toHaveBeenCalledTimes(2);
-        expect(category.name).toBe('Other movie');
-        expect(category.description).toBe('Other movie category');
-    });
-
-    test('activate method', () => {
-        let category = new Category({name: 'Movie', isActive: false});
-        category.activate();
-        expect(category.isActive).toBeTruthy();
-
-        category = new Category({name: 'Movie', isActive: true});
-        category.activate();
-        expect(category.isActive).toBeTruthy();
-    });
-
-    test('deactivate method', () => {
-        let category = new Category({name: 'Movie', isActive: false});
-        category.deactivate();
-        expect(category.isActive).toBeFalsy();
-
-        category = new Category({name: 'Movie', isActive: true});
-        category.deactivate();
-        expect(category.isActive).toBeFalsy();
-    });
-
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,65 +1,31 @@
-import UniqueEntityId from "../../../@shared/domain/value-objects/unique-entity-id";
-import Entity from "../../../@shared/domain/entitties/entity";
-import ValidatorRules from "../../../@shared/domain/validators/validator-rules";
-import CategoryValidatorFactory from "../validators/category.validator";
-
 export type CategoryProperties = {
-    name: string;
-    isActive?: boolean;
-    description?: string;
-    createdAt?: Date;
+    name: string,
+    description?: string,
+    isActive?: boolean,
+    createdAt?: Date,
 }
+export default class Category {
 
-export default class Category extends Entity<CategoryProperties> {
-
-    constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    constructor(public readonly props: CategoryProperties) {
         Category.validate(props);
-        super(props, id);
-        this.description = this.props.description ?? null;
-        this.props.isActive = this.props.isActive ?? true;
-        this.props.createdAt = this.props.createdAt ?? new Date();
+        this.description = this.props.description;
+        this.isActive = this.props.isActive ?? true;
+        this.createdAt = this.props.createdAt ?? new Date();
     }
 
-    update(name: string, description: string): void {
-        Category.validate({ name, description });
-        this.name = name;
-        this.description = description;
-    }
-
-    static validate(props: CategoryProperties): void {
-        const validator = CategoryValidatorFactory.create();
-        validator.validate(props);
-    }
-
-    activate(): void {
-        this.props.isActive = true;
-    }
-
-    deactivate(): void {
-        this.props.isActive = false;
-    }
-
-    get name(): string {
-        return this.props.name;
-    }
-
-    private set name(value: string) {
-        this.props.name = value;
-    }
-
-    get isActive(): boolean {
-        return this.props.isActive;
-    }
-
-    get description(): string {
-        return this.props.description;
+    static validate(props: CategoryProperties) {
+        return true; //todo: implement validate
     }
 
     private set description(value: string) {
         this.props.description = value ?? null;
     }
 
-    get createdAt(): Date | undefined {
-        return this.props.createdAt;
+    private set isActive(value: boolean) {
+        this.props.isActive = value;
+    }
+
+    private set createdAt(value: Date) {
+        this.props.createdAt = value;
     }
 }
